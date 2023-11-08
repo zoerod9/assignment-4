205173fd2371dee9edfdd5dd46e8777c5c0e4f91
Zoe Rodriguez <zoe.rod9767@gmail.com>
2023-11-07 20:48:01 -0600

    part 1 done

--- makefile
+++ makefile
@@ -4,10 +4,13 @@ CFLAGS = -Wall -g
 all: part1 
 
 clean:
-	rm -f *.o part1 
+	rm -f *.o part1 part1-out-test part1-output
 
 part1: part1.c
 	$(CC) $(CFLAGS) -o part1 part1.c
 
-run: part1
-	./part1
+run-test: part1
+	./part1 part1testsequence part1-out-test
+
+run-1: part1
+	./part1 part1sequence part1-output
--- part1.c
+++ part1.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
-
-int main(int argc, char *arvg[])
+int main(int argc, char *argv[])
 {
-    puts("hello worldn");
+    char *infileName = argv[1];
+    char *outfileName = argv[2];
+
+    unsigned long LA, PA;
+    unsigned long p = 5, f = 3, d = 7;
+    unsigned long pnum, fnum, dnum;
+
+    int PT[8] = {2, 4, 1, 7, 3, 5, 6, -1};
+
+    FILE *infile = fopen(infileName, "rb");
+    FILE *outfile = fopen(outfileName, "wb");
+
+    // read LA into variable LA
+    while (fread(&LA, sizeof(unsigned long), 1, infile) == 1)
+    {
+        // convert LA to PA
+        dnum = LA & 0x7F;
+        pnum = LA >> d;
+        fnum = PT[pnum];
+        PA = (fnum << d) + dnum;
+
+        // printf("LA = %lx   PA = %lx \n", LA, PA);
+
+        // write PA to outfile
+        fwrite(&PA, sizeof(unsigned long), 1, outfile);
+    }
+
+    fclose(infile);
+    fclose(outfile);
 }
